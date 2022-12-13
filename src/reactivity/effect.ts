324ff8b73f2d4b0809f67c5f8153d10c43d7ed65
aoshien <aoshisen@fanxiapp.com>
2022-12-13 18:12:03 +0800

    fix: 修复effect的触发依赖的逻辑

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -6,20 +6,21 @@ class ReactiveEffect {
   run() {
     activeEffect = this;
     this._fn();
+    //实现调用run方法的时候需要得到fn的返回值
+    return this._fn();
   }
 }
 //map 对象就像是一个对象，但是这个对象里面的键可以是任何类型的属性
-const targetMap = new Map();
+let targetMap = new Map();
 export function track(target, key) {
   //取到target 上面存的key值
   let depsMap = targetMap.get(target);
 
   if (!depsMap) {
     depsMap = new Map();
-    depsMap.set(target, depsMap);
+    targetMap.set(target, depsMap);
   }
-  let dep = targetMap.get(key);
-
+  let dep = depsMap.get(key);
   if (!dep) {
     dep = new Set();
     //这里初始化的时候dep就是空
@@ -32,15 +33,17 @@ export function track(target, key) {
   //然后把 target key 对应起来
   //target=> key => dep
 }
-let activeEffect;
-export function effect(fn) {
-  let _effect = new ReactiveEffect(fn);
-  _effect.run();
-}
 export function trigger(target, key) {
   let depsMap = targetMap.get(target);
   let dep = depsMap.get(key);
   for (const effect of dep) {
     effect.run();
   }
+}
+let activeEffect;
+export function effect(fn) {
+  let _effect = new ReactiveEffect(fn);
+  _effect.run();
+  //以当前这个effect的实例作为run 方法的this的指向
+  return _effect.run.bind(_effect);
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -3,7 +3,6 @@ import { effect } from "../effect";
 describe("effect", () => {
   it("happy path", () => {
     //创建一个响应式对象
-
     const user = reactive({
       age: 10,
     });
@@ -18,8 +17,25 @@ describe("effect", () => {
     //期望响应式对象的值为11
     expect(nextAge).toBe(11);
     //update
-    // user.age++;
+    user.age++;
     // //期望effect包裹的函数能够在user的set操作完成之后触发
-    // expect(nextAge).toBe(12);
+    expect(nextAge).toBe(12);
   });
+
+  it.skip("should return runner when call effect",()=>{
+    //我们调用effect（fn）之后是会返回一个function的runner ，当调用这个function(runner) 的时候，就会再执行一下这个effect 函数，并且这个时候会返回effect 函数执行的值的
+    let foo =10;
+    //希望effect 函数执行之后返回的是一个runner函数
+    const runner=effect(()=>{
+      foo++;
+      return "foo";
+    })
+    //期待第一次的effect 函数是会调用传递进去的fn的
+    expect(foo).toBe(11)
+    //我们把返回函数执行一遍，期待传递进去的函数再次被调用
+    const r=runner()
+    expect(foo).toBe(12)
+    //然后我们还期待runner函数的返回值是传递进去的函数的 返回值
+    expect(r).toBe("foo")
+  })
 });
