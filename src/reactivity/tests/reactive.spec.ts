f30bc9cc688afdb81101927c5b325ffc56db1dec
aoshisen <aoshisen23@gmail.com>
2022-12-19 19:50:27 +0800

    feat: isReactive 和 IsReadonly函数的实现

--- src/reactivity/tests/reactive.spec.ts
+++ src/reactivity/tests/reactive.spec.ts
@@ -1,11 +1,15 @@
-import {reactive} from "../reactive"
-describe("reactive",()=>{
-    it("happy path",()=>{
-        const original={foo:10};
-        const observer=reactive(original)
-        //创建响应式对象，希望响应式对象能读取到之前的值
-        expect(observer.foo).toBe(10)
-        //希望响应式对象和原来的值是两个不同的引用
-        expect(observer).not.toBe(original)
-    })
-})
+import { IsReactive, reactive } from "../reactive";
+describe("reactive", () => {
+  it("happy path", () => {
+    const original = { foo: 10 };
+    const observer = reactive(original);
+    //创建响应式对象，希望响应式对象能读取到之前的值
+    expect(observer.foo).toBe(10);
+    //希望响应式对象和原来的值是两个不同的引用
+    expect(observer).not.toBe(original);
+    //IsReactive
+    expect(IsReactive(original)).toBe(false);
+
+    expect(IsReactive(observer)).toBe(true);
+  });
+});
