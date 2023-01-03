c84cc04fe9fb832e3d8c339eeb3e598962710e3c
aoshien <aoshisen@fanxiapp.com>
2023-01-03 18:15:26 +0800

    refactor: 增加一些注释

--- src/runtime-core/createApp.ts
+++ src/runtime-core/createApp.ts
@@ -1,8 +1,13 @@
 import {createVNode} from './vnode'
 import {render} from './renderer'
 
+// 接收一个rootComponent 然后返回一个对象 里面有mount方法
+//具体用法就像下面
+//const rootContainer=document.getElementById("app")
+//createApp(App).mount(rootContainer)
 export function createApp(rootComponent){
-
+    // 这时候的rootComponent 还是初始的状态
+    // 就是一个对象里面有render方法，和setup方法等
     return {
         //接收一个根容器，在页面上存在的元素节点
         mount(rootContainer){
