ae7a6b9972ba21e46349ca20399180fdce60d511
aoshien <aoshisen@fanxiapp.com>
2023-02-06 15:30:02 +0800

    feat: 实现transform 功能以及插件的功能

---
+++ src/complier-core/src/transform.ts
@@ -0,0 +1,32 @@
+export function transform(root: any, options: any) {
+  const context = createTransformContext(root, options);
+  travelNode(root, context);
+}
+
+function createTransformContext(root: any, options: any) {
+  const context = {
+    root,
+    nodeTransforms: options.nodeTransforms || [],
+  };
+  return context;
+}
+
+function travelNode(node: any, context) {
+  const nodeTransforms = context.nodeTransforms;
+  for (let i = 0; i < nodeTransforms.length; i++) {
+    const nodeTransform = nodeTransforms[i];
+    nodeTransform(node);
+  }
+
+  travelChildren(node, context);
+}
+
+function travelChildren(node: any, context: any) {
+  const children = node.children;
+  if (children) {
+    for (let i = 0; i < children.length; i++) {
+      const node = children[i];
+      travelNode(node, context);
+    }
+  }
+}
